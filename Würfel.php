729906d61dd77e646fd9a18da15176d5cef8d09f
Moussakassem <80769347+Moussakassem@users.noreply.github.com>
2021-08-18 16:48:58 +0200

    Add files via upload

---
+++ Würfel.php
@@ -0,0 +1,22 @@
+<?php
+/// Script start
+
+/// Creates variable $dice and sets the content to a random number between the 1 and 6
+$dice  = rand(1,6);
+
+
+/// Display $dice 
+echo $dice ;
+
+/// If $dice is a 6 then display echo message
+if($dice == 6) {
+echo "Good Job! you threw a 6";
+}
+
+/// If $dice is lower then 3 display echo message
+if($dice < 3) {
+echo "Don't give up try again!";
+}
+
+/// Script end
+?>
