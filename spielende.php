bafec2b3b92b02084e3944df3939bfaff501d1c4
fisi-pr <fisi.pr@pm.me>
2021-09-07 19:06:12 +0200

    vorletzter_commit

---
+++ spielende.php
@@ -0,0 +1,9 @@
+<?php
+include("kniffel_aufgabe_alpha.php");
+
+if($gesamtergebnis1>$gesamtergebnis2) {
+    echo "Herzlichen Glückwunsch " . $Spieler1;
+}
+else{
+    echo "Herzlichen Glückwunsch " . $Spieler1;
+}
